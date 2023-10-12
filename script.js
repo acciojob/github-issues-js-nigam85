89fd215bb2ebe21fd62fc000daa9b148f164cf96
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-12 14:26:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-//your code here
+let currentPage = 1;
+
+const loadIssues = async () => {
+  try {
+    const response = await fetch(`https://api.github.com/repositories/1296269/issues?page=${currentPage}&per_page=5`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch issues.');
+    }
+
+    const data = await response.json();
+    const issueList = document.getElementById('issue-list');
+    issueList.innerHTML = '';
+
+    data.forEach((issue) => {
+      const issueItem = document.createElement('li');
+      issueItem.textContent = issue.title;
+      issueList.appendChild(issueItem);
+    });
+
+    document.getElementById('page-number').textContent = `Page number ${currentPage}`;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+document.getElementById('load_prev').addEventListener('click', () => {
+  if (currentPage > 1) {
+    currentPage--;
+    loadIssues();
+  }
+});
+
+document.getElementById('load_next').addEventListener('click', () => {
+  currentPage++;
+  loadIssues();
+});
+
+loadIssues();
